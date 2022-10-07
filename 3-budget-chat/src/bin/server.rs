e1fef20d4d07b54eda0d78461b935f772d9f967e
Leonardo Fedalto <lfedalto@gmail.com>
2022-10-07 23:29:43 +0200

    Move main.rs to bin/server.rs

--- 3-budget-chat/src/bin/server.rs
+++ 3-budget-chat/src/bin/server.rs
@@ -5,7 +5,6 @@ use budget_chat::server::Server;
 #[tokio::main]
 async fn main() -> Result<()> {
     let address = "0.0.0.0:9003";
-    // let listener = TcpListener::bind(address).await?;
     let server = Server::new(address).await?;
 
     server.run().await;
